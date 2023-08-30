7b29f25855e806283140585ebe98d4db18a4a698
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-08-29 22:41:06 -0400

    Inicio curso PG

---
+++ scripts/position-substring.sql
@@ -0,0 +1,6 @@
+-- Devuelve nombre y apellido si el nombre es 'Sebastian Rodriguez'
+SELECT
+  name,
+  SUBSTRING(name, 0, POSITION(' ' in name) ) AS first_name,
+  SUBSTRING(name, POSITION(' ' in name) )AS last_name
+FROM users;
