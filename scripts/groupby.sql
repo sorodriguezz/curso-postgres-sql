0b7d0b670aa0103e1b8c6b3caafa5ee01b35ba5f
soRodriguezz <s.rodriguezzapata01@gmail.com>
2023-08-30 15:43:54 -0400

    funciones para group by

--- scripts/groupby.sql
+++ scripts/groupby.sql
@@ -7,3 +7,12 @@ WHERE
   followers = 4 OR followers = 4999
 GROUP BY
   followers;
+
+
+
+select
+    count(*) as cantidad,
+    substring(email, position('@' in email) +1 ) as domain
+from users
+GROUP BY domain
+order by cantidad desc;
